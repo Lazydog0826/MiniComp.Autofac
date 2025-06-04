d7c495d11006816100ad08a896fbdaffcac7119b
huangzicheng <770233343@qq.com>
2025-06-04 16:32:02 +0800

    init

---
+++ MiniComp.Autofac/MiniComp.Autofac/AutofacDependencyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MiniComp.Autofac;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class AutofacDependencyAttribute(Type type) : Attribute
+{
+    public ServiceLifetime ServiceLifetime { get; set; } = ServiceLifetime.Scoped;
+
+    public Type Type { get; set; } = type;
+
+    public string ServiceKey { get; set; } = string.Empty;
+}
---
+++ MiniComp.Autofac/MiniComp.Autofac/Setup.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MiniComp.Core.Extension;
+
+namespace MiniComp.Autofac;
+
+public static class Setup
+{
+    /// <summary>
+    /// 使用Autofac
+    /// </summary>
+    /// <param name="hostBuilder"></param>
+    public static void UseAutofac(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    }
+
+    /// <summary>
+    /// 自动注入
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="types"></param>
+    public static void AutoAddDependency(this IServiceCollection services, List<Type> types)
+    {
+        types.ForEach(d =>
+        {
+            var at = d.GetCustomAttribute<AutofacDependencyAttribute>();
+            if (at == null)
+                return;
+            if (string.IsNullOrWhiteSpace(at.ServiceKey))
+            {
+                services.Inject(at.ServiceLifetime, at.Type, d);
+            }
+            else
+            {
+                services.Inject(at.ServiceKey, at.ServiceLifetime, at.Type, d);
+            }
+        });
+    }
+}
